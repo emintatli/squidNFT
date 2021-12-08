870ae157e4220f4d5f21c56b62301898e96b8897
emintatli <asasautorun12@gmail.com>
2021-12-08 18:33:18 +0300

    asdsa

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
   const connectHandler=async()=>{
     try{
       const accounts=await window.ethereum.request({ method: 'eth_requestAccounts' });
-      await window.ethereum.request({ method: 'wallet_switchEthereumChain', params:[{chainId: '0x38'}]});
       const provider=await detectEthereumProvider();
       const userWallet=accounts[0];
        const contract=await loadContract("BSCSquidPunks",provider);
